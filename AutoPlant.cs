e914450962fd476b6d3c9e3e50c130d636b9acac
MON@H <MONaH.Rasta@gmail.com>
2024-04-05 11:28:38 +0300

    v1.1.0

---
+++ AutoPlant.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using Facepunch;
+using System.Linq;
+
+namespace Oxide.Plugins {
+    [Info("Auto Plant", "Egor Blagov / rostov114", "1.1.0")]
+    [Description("Automation of your plantations")]
+    class AutoPlant : RustPlugin {
+
+        #region Configuration
+        private Configuration _config;
+        public class Configuration
+        {
+            [JsonProperty(PropertyName = "Auto Plant permission")]
+            public string autoPlant = "autoplant.use";
+
+            [JsonProperty(PropertyName = "Auto Gather permission")]
+            public string autoGather = "autoplant.gather.use";
+
+            [JsonProperty(PropertyName = "Auto Cutting permission")]
+            public string autoCutting = "autoplant.cutting.use";
+
+            [JsonProperty(PropertyName = "Auto Remove Dying permission")]
+            public string autoDying = "autoplant.removedying.use";
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            try
+            {
+                _config = Config.ReadObject<Configuration>();
+            }
+            catch
+            {
+                PrintError("Error reading config, please check!");
+
+                Unsubscribe(nameof(OnGrowableGather));
+                Unsubscribe(nameof(CanTakeCutting));
+                Unsubscribe(nameof(OnRemoveDying));
+                Unsubscribe(nameof(OnEntityBuilt));
+            }
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            _config = new Configuration();
+            SaveConfig();
+        }
+
+        protected override void SaveConfig() => Config.WriteObject(_config);
+        #endregion
+
+        #region Oxide Hooks
+        private void Init() 
+        {
+            permission.RegisterPermission(_config.autoPlant, this);
+            permission.RegisterPermission(_config.autoGather, this);
+            permission.RegisterPermission(_config.autoCutting, this);
+            permission.RegisterPermission(_config.autoDying, this);
+        }
+
+        private object OnGrowableGather(GrowableEntity plant, BasePlayer player)
+        {
+            if (!this.CheckUsing(player, plant, _config.autoGather))
+                return null;
+
+            PlanterBox planterBox = plant.GetParentEntity() as PlanterBox;
+
+            Unsubscribe(nameof(OnGrowableGather));
+            foreach (GrowableEntity growable in planterBox.children.ToList())
+            {
+                growable.PickFruit(player);
+            }
+            Subscribe(nameof(OnGrowableGather));
+
+            return true;
+        }
+
+        private object CanTakeCutting(BasePlayer player, GrowableEntity plant)
+        {
+            if (!this.CheckUsing(player, plant, _config.autoCutting))
+                return null;
+
+            PlanterBox planterBox = plant.GetParentEntity() as PlanterBox;
+
+            Unsubscribe(nameof(CanTakeCutting));
+            foreach (GrowableEntity growable in planterBox.children.ToList())
+            {
+                if (growable.CanClone())
+                {
+                    growable.TakeClones(player);
+                }
+            }
+            Subscribe(nameof(CanTakeCutting));
+
+            return true;
+        }
+
+        private object OnRemoveDying(GrowableEntity plant, BasePlayer player)
+        {
+            if (!this.CheckUsing(player, plant, _config.autoDying))
+                return null;
+
+            PlanterBox planterBox = plant.GetParentEntity() as PlanterBox;
+
+            Unsubscribe(nameof(OnRemoveDying));
+            foreach (GrowableEntity growable in planterBox.children.ToList())
+            {
+                growable.RemoveDying(player);
+            }
+            Subscribe(nameof(OnRemoveDying));
+
+            return true;
+        }
+
+        private void OnEntityBuilt(Planner plan, GameObject seed) 
+        {
+            BasePlayer player = plan.GetOwnerPlayer();
+            GrowableEntity plant = seed.GetComponent<GrowableEntity>();
+            if (player == null || plant == null || !permission.UserHasPermission(player.UserIDString, _config.autoPlant))
+                return;
+
+            NextTick(() => 
+            {
+                Item held = player.GetActiveItem();
+                if (held == null || held.amount == 0)
+                    return;
+
+                if (player.serverInput.IsDown(BUTTON.SPRINT) && plant.GetParentEntity() is PlanterBox)
+                {
+                    PlanterBox planterBox = plant.GetParentEntity() as PlanterBox;
+                    Construction construction = PrefabAttribute.server.Find<Construction>(plan.GetDeployable().prefabID);
+                    List<Construction.Target> targets = Pool.GetList<Construction.Target>();
+                    foreach (Socket_Base sock in PrefabAttribute.server.FindAll<Socket_Base>(planterBox.prefabID)) 
+                    {
+                        if (!sock.female)
+                            continue;
+
+                        Vector3 socketPoint = planterBox.transform.TransformPoint(sock.worldPosition);
+                        Construction.Target target = new Construction.Target();
+
+                        target.entity = planterBox;
+                        target.ray = new Ray(socketPoint + Vector3.up * 1.0f, Vector3.down);
+                        target.onTerrain = false;
+                        target.position = socketPoint;
+                        target.normal = Vector3.up;
+                        target.rotation = new Vector3();
+                        target.player = player;
+                        target.valid = true;
+                        target.socket = sock;
+                        target.inBuildingPrivilege = true;
+
+                        if (!this.IsFree(construction, target))
+                            continue;
+
+                        targets.Add(target);
+                    }
+
+                    Unsubscribe(nameof(OnEntityBuilt));
+                    foreach (Construction.Target target in targets) 
+                    {
+                        plan.DoBuild(target, construction);
+                        if (held.amount == 0)
+                            break;
+                    }
+                    Subscribe(nameof(OnEntityBuilt));
+
+                    Pool.FreeList(ref targets);
+                }
+            });
+        }
+        #endregion
+
+        #region Helpers
+        public bool IsFree(Construction common, Construction.Target target) 
+        {
+            List<Socket_Base> list = Facepunch.Pool.GetList<Socket_Base>();
+            common.FindMaleSockets(target, list);
+            Socket_Base socketBase = list[0];
+            Facepunch.Pool.FreeList(ref list);
+            return !target.entity.IsOccupied(target.socket) && socketBase.CheckSocketMods(socketBase.DoPlacement(target));
+        }
+
+        public bool CheckUsing(BasePlayer player, GrowableEntity plant, string perm)
+        {
+            if (player == null || plant == null || !permission.UserHasPermission(player.UserIDString, perm))
+                return false;
+
+            if (player.serverInput.IsDown(BUTTON.SPRINT) && plant.GetParentEntity() is PlanterBox)
+                return true;
+
+            return false;
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # AutoPlant
+
 Oxide plugin for Rust. Automation of your plantations.
+Players with permission are able to plant whole planterbox in one click. To do that you should:
+
+1. Select plant/clone/seed to be your active item
+2. Target planterbox
+3. **HOLD SHIFT (SPRINT button)** and click mouse button as you usually plant
+4. All free slots in planterbox will be planted by active item (if you have enough items in active stack).
+
+Also your players can gather / cutting / remove dying using the **SHIFT (SPRINT button)** key.
+
+To move fertilizer automatically, just set active item and click mouse button.
+
+## Permissions
+
+### Default permissions
+
+- `autoplant.use` -- permission to use auto planting
+- `autoplant.gather.use` -- permission to use auto gather
+- `autoplant.cutting.use` -- permission to use auto cutting
+- `autoplant.removedying.use` -- permission to use auto remove dying
+- `autoplant.fertilizer.use` -- permission to use auto move fertilizer
+
+## Chat Commands
+
+- `/fertilizer` -- Shows amount move fertilizer
+- `/fertilizer <amount>` -- Set amount move fertilizer
+
+## Configuration
+
+### Default Configuration
+
+```json  
+{
+  "Auto Plant permission": "autoplant.use",
+  "Auto Gather permission": "autoplant.gather.use",
+  "Auto Cutting permission": "autoplant.cutting.use",
+  "Auto Remove Dying permission": "autoplant.removedying.use",
+  "Auto fertilizer permission": "autoplant.fertilizer.use",
+  "Auto fertilizer configuration": {
+    "Maximum distance from the PlanterBox": 5,
+    "Default fertilizer amount": 100
+  }
+}
+```  
+
+## Credits
+
+- **Egor Blagov** - original author
+
+### Special thanks
+
+- **@Sharovoz** - Sponsoring plugin development
